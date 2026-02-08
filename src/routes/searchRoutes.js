3c089ace4317ef7d8cde58761eacb4040efd33de
Alfred <alfiewayne99@gmail.com>
2026-02-08 19:53:29 +0400

    Fix import path in searchRoutes.js

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -6,7 +6,7 @@
 const express = require('express');
 const router = express.Router();
 const { asyncHandler } = require('../middleware/errorHandler');
-const { NexvoyPriceEngine } = require('../../../src/index');
+const { NexvoyPriceEngine } = require('../index');
 const {
   ValidationError,
   MissingFieldError,
